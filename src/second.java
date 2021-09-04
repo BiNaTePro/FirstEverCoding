56d86f025864eb6373c4c58f8f0deb7209f2a152
Bishka <bishka@Bishkas-MacBook-Pro.local>
2021-09-03 19:40:01 -0700

    Initial commit

---
+++ src/second.java
@@ -0,0 +1,21 @@
+
+public class second {
+    // variable types
+    public static void main (String[] args){
+        //declare --> zarlan tunhaglah
+        int x = 15;
+        double y = 15.6;
+        char ch = 'a';
+        String word = "hello world";
+        boolean check = true;
+
+        int a = 15;
+        int b = 20;
+        int sum = a+b;
+        int something= a*b+sum-a/b;
+        int something1 = a/b;
+
+
+        System.out.println("a+b=" +something1);
+    }
+}
